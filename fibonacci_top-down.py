e20928a50906884eb980bb8c826e170d758efd3d
analyst_Jung <myeonghoon01@gmail.com>
2024-10-29 00:14:21 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   fibonacci_bottom-up.py
    	modified:   fibonacci_top-down.py

--- fibonacci_top-down.py
+++ fibonacci_top-down.py
@@ -1,9 +1,12 @@
 def fibonacci(n, memo):
     if n <= 1:
         return n
+    
     if memo[n] != -1:
         return memo[n]
+    
     memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
+    
     return memo[n]
 
 n = 10
